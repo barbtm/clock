66b6905d0bd728738d51b20a2787934f832a5eac
barbtm <128565098+barbtm@users.noreply.github.com>
2023-08-15 14:33:27 -0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const hour = document.querySelector('#hours');
+const min = document.querySelector('#minutes');
+const sec = document.querySelector('#seconds');
 
+setInterval( () => {
+    let date = new Date(); 
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    document.getElementById('hours').innerHTML = formatTime(hours);
+    document.getElementById('minutes').innerHTML = formatTime(minutes);
+    document.getElementById('seconds').innerHTML = formatTime(seconds);
+}, 1000);
+
+function formatTime (time){
+    return time < 10 ? '0' + time : time;
+}
